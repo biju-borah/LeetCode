c421afc2c966a8034f685253e4f47490edd50746
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-07-05 09:11:07 +0530

    Leetcoding

--- Arrays/Easy/RunningSum.java
+++ Arrays/Easy/RunningSum.java
@@ -1,7 +1,20 @@
 package Arrays.Easy;
 
 public class RunningSum {
-    public static void main(String[] args) {
+    public static int[] runningSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        for (int i : nums) {
+            System.out.println(i);
+        }
+        return nums;
+    }
 
+    public static void main(String[] args) {
+        int[] nums = new int[] { 3, 1, 2, 10, 1 };
+        runningSum(nums);
     }
 }
