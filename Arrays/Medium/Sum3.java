2a8a4ae142b75b0d91cb7b62e69600e89e04af3c
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-08-14 11:34:59 +0530

    Leetcoding

--- Arrays/Medium/Sum3.java
+++ Arrays/Medium/Sum3.java
@@ -7,15 +7,18 @@ public class Sum3 {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            List<Integer> list = new ArrayList<>();
             int j = i + 1;
             int k = nums.length - 1;
             while (j < k) {
+                List<Integer> list = new ArrayList<>();
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
+                if (sum == 0 && i != j && i != k && j != k) {
                     list.add(nums[i]);
                     list.add(nums[j]);
                     list.add(nums[k]);
+                    if (!result.contains(list)) {
+                        result.add(list);
+                    }
                     j++;
                     k--;
                 } else if (sum < 0) {
@@ -24,11 +27,33 @@ public class Sum3 {
                     k--;
                 }
             }
+
         }
+        // for (List<Integer> l : result) {
+        // for (int x : l) {
+        // System.out.println(x);
+        // }
+        // }
         return result;
     }
 
     public static void main(String[] args) {
+        int[] arr = new int[] { -4, -1, -1, 0, 1, 2 };
+        Sum3.threeSum(arr);
+        // ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        // ArrayList<Integer> arr = new ArrayList<>();
 
+        // arr.add(1);
+        // arr.add(2);
+        // arr.add(3);
+        // list.add(arr);
+        // ArrayList<Integer> arr2 = new ArrayList<>();
+        // arr2.add(1);
+        // arr2.add(2);
+        // arr2.add(3);
+
+        // if (list.contains(arr2)) {
+        // System.out.println("woh");
+        // }
     }
 }
