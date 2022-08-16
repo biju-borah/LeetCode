7717b898ef099c64b701620458d3b12ae8f99dc2
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-08-16 23:19:31 +0530

    Leetcoding .....

--- Arrays/Medium/SubArraySum.java
+++ Arrays/Medium/SubArraySum.java
@@ -1,54 +1,28 @@
 package Arrays.Medium;
 
+import java.util.HashMap;
+
 public class SubArraySum {
 
-    public static int sum(int[] arr, int i, int j) {
-        int sum = 0;
-        if (i == 0) {
-            sum = arr[j - 1] - 0;
-        } else {
-            sum = arr[j - 1] - arr[i - 1];
-        }
-        return sum;
-    }
-
-    public static int helper(int[] nums, int k, int start, int end, int count) {
-        // if (end == nums.length) {
-        // return count;
-        // } else if (start > end) {
-        // count = helper(nums, k, 0, end + 1, count);
-        // } else {
-        // if (sum(nums, start, end + 1) == k) {
-        // count++;
-        // }
-        // count = helper(nums, k, start + 1, end, count);
-        // }
-        // return count;
-        int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k)
-                    res++;
-            }
-        }
-        return res;
-
-    }
-
     public static int subarraySum(int[] nums, int k) {
-        // int[] prefixSum = new int[nums.length];
-        // prefixSum[0] = nums[0];
-        // for (int i = 1; i < nums.length; i++) {
-        // prefixSum[i] = prefixSum[i - 1] + nums[i];
-        // }
 
-        return helper(nums, k, 0, 0, 0);
+        HashMap<Integer, Integer> hMap = new HashMap<>();
+        hMap.put(0, 1);
+        int curSum = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curSum += nums[i];
+            System.out.println(curSum - k);
+            if (hMap.containsKey(curSum - k)) {
+                count += hMap.get(curSum - k);
+            }
+            hMap.put(curSum, hMap.getOrDefault(curSum, 0) + 1);
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[] { -1, -1, 1 };
+        int[] arr = new int[] { 1, -1, 0 };
         // subarraySum(arr, 2);
         System.out.println(subarraySum(arr, 0));
     }
