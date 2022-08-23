c265bdf8613b7b4ab09928895b5de5cc42f9ff10
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-08-23 20:04:07 +0530

    Update SortColors.java

--- Arrays/Medium/SortColors.java
+++ Arrays/Medium/SortColors.java
@@ -1,7 +1,30 @@
 package Arrays.Medium;
 
 public class SortColors {
+    public static int findMax(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max)
+                max = nums[i];
+        }
+        return max;
+    }
     public static void sortColors(int[] nums) {
+        int max = findMax(nums);
+        int[] count = new int[max + 1];
+        for (int i = 0; i < count.length; i++) {
+            count[i] = 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                nums[index++] = i;
+                count[i]--;
+            }        
+        }
 
     }
 
